9c8cc412a474c8ec546dbcf172b6ce81efb7a13d
Kazcarob <83021307+Cau312@users.noreply.github.com>
2021-07-17 19:24:47 -0300

    Começo do Projeto
    Só o arquivo bruto do projeto em Java

---
+++ src/application/Main.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		String comeco = sc.nextLine();
+		
+		System.out.println(comeco);
+		
+		sc.close();
+		
+	}
+
+}
