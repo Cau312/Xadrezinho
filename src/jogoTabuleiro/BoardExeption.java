bbbf51ee33e3443489f66ebd10a73a52203ee9c0
Kazcarob <83021307+Cau312@users.noreply.github.com>
2021-07-31 20:03:01 -0300

    Xadrezinho V0.8.1 (dnv)
    O commit passado deu errado
    Adicionado Movimento Especial do Rei

--- src/jogoTabuleiro/BoardExeption.java
+++ src/jogoTabuleiro/BoardExeption.java
@@ -3,9 +3,8 @@ package jogoTabuleiro;
 public class BoardExeption extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
-	
 	public BoardExeption(String msg) {
 		super(msg);
 	}
-	
+
 }
--- src/xadrez/ChessPosition.java
+++ src/xadrez/ChessPosition.java
@@ -4,12 +4,16 @@ import jogoTabuleiro.Position;
 
 public class ChessPosition {
 
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_RESET = "\u001B[0m";
+	
 	private char colunas;
 	private int linhas;
 
 	public ChessPosition(int linhas, char colunas) {
 		if (colunas < 'a' || colunas > 'h' || linhas < 1 || linhas > 8) {
-			throw new ChessException("Valores Invalidos [1 - 8] e [a - h]");
+			System.out.println(ANSI_RED + "Valores Invalidos :( | Valore Validos: [1 - 8] e [a - h]");
+			throw new ChessException("ChessPosition - public ChessPosition" + ANSI_RESET);
 		}
 		this.colunas = colunas;
 		this.linhas = linhas;
@@ -27,14 +31,13 @@ public class ChessPosition {
 		return new Position(8 - linhas, colunas - 'a');
 	}
 
-	protected static ChessPosition toChessPosition(Position posi) {
-		return new ChessPosition((8 - posi.getLinha()), (char)('a' - posi.getColuna()));
+	protected static ChessPosition fromPosition(Position posi) {
+		return new ChessPosition((8 - posi.getLinha()), (char) ('a' + posi.getColuna()));
 	}
 
 	@Override
 	public String toString() {
 		return "" + colunas + linhas;
 	}
-	
 
 }
